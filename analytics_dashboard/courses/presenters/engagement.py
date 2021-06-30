06c25969feba8d7efe166dd0293cf1103136d682
he-kefen <69139914+he-kefen@users.noreply.github.com>
2021-06-30 16:12:58 +0800

    changed module type to video_jwplayer
    Insights module is not able to handle multiple module type for 'video' courseware

--- analytics_dashboard/courses/presenters/engagement.py
+++ analytics_dashboard/courses/presenters/engagement.py
@@ -161,7 +161,7 @@ class CourseEngagementVideoPresenter(CourseAPIPresenterMixin, CoursePresenter):
 
     @property
     def module_type(self):
-        return 'video'
+        return 'video_jwplayer'#'video'
 
     def fetch_course_module_data(self):
         # Get the videos from the API.  Use course_module_data() for cached data.
